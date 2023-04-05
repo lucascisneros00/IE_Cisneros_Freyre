58da3abf85a0bd7723f7357081feef8cdd9419f7
Lucas Cisneros <lucascisneros00@gmail.com>
2023-04-05 16:43:46 -0500

    randomize groups and ids

--- dictator_v1/app_v1/__init__.py
+++ dictator_v1/app_v1/__init__.py
@@ -2,15 +2,7 @@ from otree.api import *
 import random
 import itertools
 import time
-
-doc = """
-One player decides how to divide a certain amount between himself and the other
-player.
-See: Kahneman, Daniel, Jack L. Knetsch, and Richard H. Thaler. "Fairness
-and the assumptions of economics." Journal of business (1986):
-S285-S300.
-"""
-
+import random
 
 class C(BaseConstants):
     NAME_IN_URL = 'dictator'
@@ -36,21 +28,34 @@ class Player(BasePlayer):
     edad = models.IntegerField(label="Introduce tu edad")
     carrera = models.StringField(
         label="Selecciona tu carrera",
-        choices=["Administración", "Contabilidad", "Derecho", "Economía", "Finanzas", "Ingenieria de la Información", "Ingeniería Empresarial", "Marketing", "Negocios Internacionales"]
+        choices=["Administración", "Contabilidad", "Derecho", "Economía", "Finanzas", "Ingenieria de la Información", "Ingeniería Empresarial", "Marketing", "Negocios Internacionales", "Otra"]
     )
-    ciclo = models.IntegerField(label="Introduce en qué ciclo te encuentras (número)")
-    distrito_de_residencia = models.StringField(label="Introduce tu distrito de residencia")
+    ciclo = models.StringField(
+        label="Selecciona el ciclo en el que estás",
+        choices=["1","2","3","4","5","6","7","8","9","10","11","12"]
+    )
+    distrito_de_residencia = models.StringField(
+        label="Selecciona tu zona de residencia",
+        choices=["Zona 1 (Puente Piedra, Comas, Carabayllo)",
+                 "Zona 2 (Independencia, Los Olivos, San Martín de Porres)",
+                 "Zona 3 (San Juan de Lurigancho)",
+                 "Zona 4 (Cercado, Rímac, Breña, La Victoria)",
+                 "Zona 5 (Ate, Chaclacayo, Lurigancho, Santa Anita, San Luis, El Agustino)",
+                 "Zona 6 (Jesús María, Lince, Pueblo Libre, Magdalena, San Miguel)",
+                 "Zona 7 (Miraflores, San Isidro, San Borja, Surco, La Molina)",
+                 "Zona 8 (Surquillo, Barranco, Chorrillos, San Juan de Miraflores)",
+                 "Zona 9 (Villa El Salvador, Villa María del Triunfo, Lurín, Pachacamác)",
+                 "Zona 10 (Callao, Bellavista, La Perla, La Punta, Carmen de la Legua, Ventanilla",
+                 "Otros"])
 
 
     treatment = models.StringField(initial='C')
     player_role = models.StringField(initial='dictator')
 
-    donation = models.CurrencyField(
-        doc="""Amount dictator decided to give""",
-        min=0,
-        max=C.ENDOWMENT,
-        label="",
+    donation = models.IntegerField(
+        min=0, max=C.ENDOWMENT, label=""
     )
+
     dictator_donation = models.CurrencyField(min=0,
         max=C.ENDOWMENT,)
     recipient_donation = models.CurrencyField(min=0,
@@ -60,20 +65,31 @@ class Player(BasePlayer):
     random_code = models.IntegerField()
 
 # FUNCTIONS
-def creating_session(subsession: Subsession):
-    # Assign players to control, t1, t2 groups
-    for i, player in enumerate(subsession.get_players()):
-        if i<len(subsession.get_players())*1/3:
-            player.treatment = 'C' #f't{i}_{player}'
-        elif i<len(subsession.get_players())*2/3:
-            player.treatment = 'T1' #f't{i}_{player}'
-        else:
-            player.treatment = 'T2' #f't{i}_{player}'
-    
-    # Generate unique ID
-    for i, p in enumerate(subsession.get_players(), start=1):
+def creating_session(subsession: Subsession): 
+
+    # Shuffle players
+    players = list(subsession.get_players())
+    random.shuffle(players)
+
+    # Give each player an ID
+    for i, p in enumerate(players, start=1):
         p.order_number = i
         p.random_code = random.randint(1,1000)
+
+    # Shuffle again
+    random.shuffle(players)
+
+    # Give players shuffled IDs
+    for i, p in enumerate(players, start=1):
+        # Assign players to control, t1, t2 groups
+        p.order_number_reshuffled = i
+        if p.order_number_reshuffled<len(players)*1/3:
+            p.treatment = 'C' 
+        elif p.order_number_reshuffled<len(players)*2/3:
+            p.treatment = 'T1' 
+        else:
+            p.treatment = 'T2' 
+        
            
 def set_payoffs(group: Group):
     for player in group.get_players():
@@ -88,7 +104,7 @@ def set_payoffs(group: Group):
     recipient.recipient_donation = recipient.donation 
 
 
-# PAGES
+# PAGES (test)
 class WaitAssign(WaitPage):
     @staticmethod
     def after_all_players_arrive(group: Group):
@@ -96,33 +112,10 @@ class WaitAssign(WaitPage):
             if player.id_in_group==1: player.player_role = 'dictator'
             if player.id_in_group==2: player.player_role = 'recipient'
 
-class ExposureControl(Page):
-
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.treatment == 'C'
-
-class ExposureT1(Page):
-
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.treatment == 'T1'
-
-class ExposureT2(Page):
-
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.treatment == 'T2'
-
-class Decision(Page):
-    form_model = 'player'
-    form_fields = ['donation']
-
-class Instructions(Page):
-    pass
-
 class TestPage(Page):
-    pass
+    @staticmethod
+    def js_vars(player: Player):
+        return dict(endowment=C.ENDOWMENT)
 
 class TestPage2(Page):
     pass
@@ -130,9 +123,8 @@ class TestPage2(Page):
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = set_payoffs
 
-class Results(Page):
-    pass
 
+## PAGES 
 class _0_Formulario(Page):
     form_model = "player"
     form_fields = ["sexo", "edad", "carrera", "ciclo", "distrito_de_residencia"]
@@ -202,13 +194,12 @@ class _10_Informacion_Final(Page):
         )
      
 page_sequence = [
-        #TestPage,TestPage2, 
+        # TestPage,TestPage2, 
         WaitAssign,
         _0_Formulario,
         _1_Instrucciones_1, _2_Instrucciones_2,
         _3_Exposure_0,
         _4_ExposureControl, _5_ExposureT1, _6_ExposureT2, 
-        WaitAssign,
         _7_Decision_C_T1,  _8_Decision_T2,
         ResultsWaitPage,
         _9_Resultados,
