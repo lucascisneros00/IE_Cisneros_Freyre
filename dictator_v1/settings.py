98190aa4e3d3a169398812210d33d0b8da81c013
Sweetdreamstome <greciabarandiaran@gmail.com>
2023-04-13 23:09:03 -0500

    add _rooms

--- dictator_v1/settings.py
+++ dictator_v1/settings.py
@@ -39,6 +39,11 @@ ROOMS = [
         display_name='Econ 101 class',
         participant_label_file='_rooms/econ101.txt',
     ),
+    dict(
+        name='e2labup',
+        display_name='E2LabUP - Room para sesiones online',
+        participant_label_file='_rooms/e2labup-room.txt',
+    ),
     dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
 ]
 
